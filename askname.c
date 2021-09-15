84b6760e5364f2c8e4fec9ae0fe868912897737c
Svetlana Dyakonova <79048669+Svdy@users.noreply.github.com>
2021-09-15 18:14:37 +0300

    Add description

--- askname.c
+++ askname.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+// Функция для считывания имени и фамилии
 void askname(char *first, char *last) {
     printf("Enter your first name: ");
     fgets(first, 255, stdin);
-    first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+    first[strlen(first)-1] = '\0'; 
     printf("Now enter your last name: ");
-    gets(last); /* buffer overflow? what's that? */
+    gets(last); 
 }
