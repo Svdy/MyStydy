5f11266afdcab77791a990f5a48158935110c8d5
Svetlana Dyakonova <svdys@yandex.ru>
2021-09-15 19:32:24 +0300

    Revert "Update main.c"
    This reverts commit bcdfac404e7a1e19b5110096396bfbd8342edc38.

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 
 #include <stdio.h>
 #include "askname.h"
-
 int main(int argc, char **argv) {
-    char firstName[255], lastName[255];
+    char first[255], last[255];
     askname(first, last);
-    printf("Hello, %s %s!\n", firstName, lastName);
+    printf("Hello, %s %s!\n", first, last);
+
     return 0;
 }
+
