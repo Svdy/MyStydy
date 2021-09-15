bcdfac404e7a1e19b5110096396bfbd8342edc38
Svetlana Dyakonova <79048669+Svdy@users.noreply.github.com>
2021-09-15 18:30:01 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 
 #include <stdio.h>
 #include "askname.h"
-int main(int argc, char **argv) {
-    char first[255], last[255];
-    askname(first, last);
-    printf("Hello, %s %s!\n", first, last);
 
+int main(int argc, char **argv) {
+    char firstName[255], lastName[255];
+    askname(first, last);
+    printf("Hello, %s %s!\n", firstName, lastName);
     return 0;
 }
-
